42db05f3cb610cabe67521aa73ea0dadcc914f22
salt <132168294+a-salt@users.noreply.github.com>
2023-09-20 16:45:26 +0100

    Challenge 18
    addition of 'about' and 'work' pages, as well as including grid layout and media queries.

---
+++ styles/case-studies.js
@@ -0,0 +1,13 @@
+//Event Listener for expanding each case study
+
+$("#toggle1").on("click",function(){
+    $("#dropDown1").slideToggle();
+});
+
+$("#toggle2").on("click",function(){
+    $("#dropDown2").slideToggle();
+});
+
+$("#toggle3").on("click",function(){
+    $("#dropDown3").slideToggle();
+});
