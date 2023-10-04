e58716b255c40761f8ab1285d0f10b6e08f032df
salt <132168294+a-salt@users.noreply.github.com>
2023-10-04 22:32:53 +0100

    Challenge 20 submission

--- styles/case-studies.js
+++ styles/case-studies.js
@@ -1,4 +1,5 @@
 //Event Listener for expanding each case study
+// change this so it closes other open case studies and just opens the one that is clicked on//
 
 $("#toggle1").on("click",function(){
     $("#dropDown1").slideToggle();
